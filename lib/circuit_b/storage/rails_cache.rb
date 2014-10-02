4767a2a7ca82fa6df8e939e1d87cf5f41d668131
Joseph Glanville <joseph.glanville@vodafone.com.au>
2014-10-02 16:07:22 +1000

    Add Rails::Cache storage backend

---
+++ lib/circuit_b/storage/rails_cache.rb
@@ -0,0 +1,31 @@
+require 'circuit_b/storage/base'
+require 'redis'
+
+module CircuitB
+  module Storage
+    class RailsCache < Base
+
+      def initialize
+        @cache = ::Rails.cache
+      end
+
+      def put(fuse_name, field, value)
+        @cache.write(key(fuse_name, field), value)
+      end
+
+      def get(fuse_name, field)
+        @cache.read(key(fuse_name, field))
+      end
+
+      def inc(fuse_name, field)
+        return @cache.increment(key(fuse_name, field))
+      end
+
+      private
+
+      def key(fuse_name, field)
+        "circuit_b:#{fuse_name}:#{field}"
+      end
+    end
+  end
+end
