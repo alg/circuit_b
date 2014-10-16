68411a7bf667f1a68c2e96b98a53eec9030e9f0d
Joseph Glanville <joseph.glanville@vodafone.com.au>
2014-10-16 17:35:31 +1100

    Run all of the E2E tests for each backend

--- test/unit/test_circuit_b.rb
+++ test/unit/test_circuit_b.rb
@@ -24,63 +24,67 @@ class TestCircuitB < Minitest::Spec
     end
   end
 
-  context 'using fuses to protect code' do
-    setup do
-      begin
-        CircuitB::Storage::Redis.new.get('dummy', 'field')
-      rescue Errno::ECONNREFUSED
-        raise 'Please start Redis on default port'
-      end
+  BACKENDS = {
+    memory: CircuitB::Storage::Memory,
+    redis: CircuitB::Storage::Redis,
+    rails_cache: CircuitB::Storage::RailsCache
+  }
 
-      CircuitB.reset_configuration
-      CircuitB.configure do |c|
-        c.state_storage = CircuitB::Storage::Redis.new
-        c.fuse 'fuse_name', allowed_failures: 1, cool_off_period: 10
-      end
-      CircuitB('fuse_name').reset
-    end
+  BACKENDS.each do |name, klass|
+    context "using fuses to protect code with backend: #{name}" do
 
-    should 'let wrap the code with fuse' do
-      executed = false
-      CircuitB('fuse_name') do
-        # Some lengthy and potentially failing operation
-        executed = true
-      end
-
-      assert executed, "Code wasn't executed"
-    end
-
-    should 'pass the execution result back' do
-      result = CircuitB('fuse_name') do
-        'result'
-      end
-      assert_equal 'result', result
-    end
-
-    should "error out if the fuse doesn't exist" do
-      begin
-        CircuitB('non_existent_fuse') do
-          # Will never be executed
-          fail 'Should never be executed'
+      setup do
+        CircuitB.reset_configuration
+        CircuitB.configure do |c|
+          c.state_storage = klass.new
+          c.fuse 'fuse_name', allowed_failures: 1, cool_off_period: 10
         end
-      rescue => e
-        assert_equal "Fuse with the name 'non_existent_fuse' is not registered", e.message
+        CircuitB('fuse_name').reset
       end
-    end
 
-    should "pass the error when it's raised by the code" do
-      begin
+      should 'let wrap the code with fuse' do
+        executed = false
         CircuitB('fuse_name') do
-          fail 'App error'
+          # Some lengthy and potentially failing operation
+          executed = true
         end
-        fail 'App error should be raised'
-      rescue => e
-        assert_equal 'App error', e.message
+
+        assert executed, "Code wasn't executed"
+      end
+
+      should 'pass the execution result back' do
+        result = CircuitB('fuse_name') do
+          'result'
+        end
+        assert_equal 'result', result
+      end
+
+      should "error out if the fuse doesn't exist" do
+        begin
+          CircuitB('non_existent_fuse') do
+            # Will never be executed
+            fail 'Should never be executed'
+          end
+        rescue => e
+          assert_equal "Fuse with the name 'non_existent_fuse' is not registered", e.message
+        end
+      end
+
+      should "pass the error when it's raised by the code" do
+        begin
+          CircuitB('fuse_name') do
+            fail 'App error'
+          end
+          fail 'App error should be raised'
+        rescue => e
+          assert_equal 'App error', e.message
+        end
+      end
+
+      should 'return the fuse if no block is given' do
+        assert CircuitB('fuse_name').is_a?(CircuitB::Fuse)
       end
     end
 
-    should 'return the fuse if no block is given' do
-      assert CircuitB('fuse_name').is_a?(CircuitB::Fuse)
-    end
   end
 end
